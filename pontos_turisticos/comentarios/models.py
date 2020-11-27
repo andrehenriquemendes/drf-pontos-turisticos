ea59d3afaccabd11c55eef4fb8538927909eba2f
Andre Mendes <andre.mendes@outlook.com>
2020-11-27 10:40:49 -0300

    deixando ViewsSets singular + endpoint comentarios

--- pontos_turisticos/comentarios/models.py
+++ pontos_turisticos/comentarios/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Comentario(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     comentario = models.TextField()
-    data = models.DateTimeField(auto_now_add=True)
-    aprovado = models.BooleanField(default=True)
+    data = models.DateTimeField(auto_now_add=True) # automatico
+    aprovado = models.BooleanField(default=True) # automatico
 
     def __str__(self):
         return self.usuario.username
