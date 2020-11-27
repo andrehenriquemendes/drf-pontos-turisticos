cc0bb08eafc429bb94ec35e6c59e1c8c4ea116e2
Andre Mendes <andre.mendes@outlook.com>
2020-11-27 12:15:21 -0300

    test: Sobrescrevendo retrieve/partial_update etc

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -15,3 +15,15 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         return Response({'Hello': request.data['nome']})
+
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        pass
