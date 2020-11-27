9e810b3f9c8ae90f003a0f62a4f846360ed4b432
Andre Mendes <andre.mendes@outlook.com>
2020-11-27 11:37:52 -0300

    test: sobrescrevendo a action GET

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
+from rest_framework.response import Response
 
 class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
@@ -8,3 +9,6 @@ class PontoTuristicoViewSet(ModelViewSet):
     # sobrescrevendo o metodo get_queryset
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    def list(self, request, *args, **kwargs):
+        return Response({'teste': 123})
