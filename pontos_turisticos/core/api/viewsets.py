49afb11f1a7ec2747e7b6afe13a9465db9d12a23
andrehenriquemendes <andre.mendes@outlook.com.br>
2020-11-27 15:01:34 -0300

    feat: habilitando SearchFilter na app core

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -1,12 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
-from core.models import PontoTuristico
-from .serializers import PontoTuristicoSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
+from core.models import PontoTuristico
+from .serializers import PontoTuristicoSerializer
 
 class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
     
+    filter_backends = (SearchFilter,)
+    search_fields = ('nome', 'descricao', 'endereco__linha1')
+    # endereco eh chave estrangeira
+    # linha1 eh um atributo do endereco
+
     # sobrescrevendo o metodo get_queryset
     def get_queryset(self):
         id = self.request.query_params.get('id', None)
